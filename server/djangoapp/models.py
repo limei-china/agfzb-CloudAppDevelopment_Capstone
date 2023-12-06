f7e0bac361abd72f5e98c70647cbde9e58b1ef39
limei-china <shen18071510@gmail.com>
2023-12-06 05:08:33 -0500

    Add CarMake and CarModel models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,7 +9,13 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
-
+class CarMake(models.Model):
+    car_name = models.CharField(null=False, max_length=30, default='john')
+    description = models.CharField(max_length=500)
+    
+    # Create a toString method for object string representation
+    def __str__(self):
+        return self.car_name + " " + self.description
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -19,8 +25,25 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    SEDAN = 'Sedan'
+    SUV = 'SUV'
+    WAGON = 'Wagon'
+    CAR_TYPE_CHOICES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
+    ]
+    # Many-To-One to Car Make model
+    model = models.ForeignKey(CarMake, null=True, on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=30, default='car')
+    dealer_id = models.IntegerField()
+    car_type = models.CharField(null=False, max_length=10, choices = CAR_TYPE_CHOICES , default=SEDAN)
+    year = models.DateField()
 
-
+    # Create a toString method for object string representation
+    def __str__(self):
+        return self.name + " " + self.dealer_id + " " + self.car_type+ " " + self.year
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
 
--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'djangobackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'MzI0MTAtc2hlbjE4',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
